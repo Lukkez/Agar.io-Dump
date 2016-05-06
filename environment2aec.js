454d650c510f31b78b0e3dc2df15ec1acdec2b75
LegitSoulja <macballerboy@gmail.com>
2016-05-06 15:46:03 -0600

    update

---
+++ environment2aec.js
@@ -0,0 +1,16 @@
+//development
+window.EnvConfig = {
+	"env_development": true,
+	"fb_app_id": '690065807764492',
+	"gplus_client_id": "686981379285-oroivr8u2ag1dtm3ntcs6vi05i3cpv0j.apps.googleusercontent.com",
+	"master_url": 'develop-direct.agar.io:8080',
+	"socketEndpoint": 'wss://mobile-dev.agario.miniclippt.com/ws',
+	"xsolla_endpoint": "https://payments-dev.agario.miniclippt.com",
+	"fb_endpoint": "https://rewardcallback-dev.agario.miniclippt.com/payments/facebook",
+	"game_url": "http://flash-web-dev1-eu-central-1.agario.miniclippt.com/",
+	"supersonic_app_key" : "44071d2d",
+	// =======================================
+	// Configurations
+	"config_url": "https://agario-configurations.s3.amazonaws.com/dev/web",
+	//"configID": "16"
+}
